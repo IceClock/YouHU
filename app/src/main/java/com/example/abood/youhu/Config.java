dfc84c025d358a4c0fb80f45ae92ab1220b51610
Abdelhadi Hussein <abod95@gmail.com>
2017-10-08 21:44:21 -0700

    Update Config.java

--- app/src/main/java/com/example/abood/youhu/Config.java
+++ app/src/main/java/com/example/abood/youhu/Config.java
@@ -4,7 +4,7 @@ package com.example.abood.youhu;
  * Created by Belal on 11/23/2015.
  */
 public class Config {
-    public static final String UPLOAD_URL= "http://ahmadkhwaja.com/abd/upvideos.php";
+    public static final String UPLOAD_URL= "";
 
     public static final String TAG_URL = "url";
     public static final String TAG_SIZE = "size";
